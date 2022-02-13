267e0dab909aef6f96a22d6243a56b373d493678
sanketp1 <psanket18052001@gmail.com>
2022-02-13 23:03:45 +0530

    first commit

---
+++ lib/Pages/coursespage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CoursesPage extends StatelessWidget {
+  const CoursesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text("This is Courses Page"),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,52 @@
+import 'package:collegebuddyweb/Pages/coursespage.dart';
+import 'package:collegebuddyweb/Pages/schedulePage.dart';
+import 'package:collegebuddyweb/Pages/dashboard.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            IconButton(
+                onPressed: () {
+                  print("Hello");
+                },
+                icon: Icon(Icons.dashboard)),
+            ListTile(
+              onTap: () {
+                print("Tapped");
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SchedulePage()));
+              },
+              leading: Icon(Icons.schedule),
+              title: Text("Schedule"),
+            ),
+            ListTile(
+              onTap: () {
+                print("Tapped");
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => CoursesPage()));
+              },
+              leading: Icon(Icons.engineering),
+              title: Text("My Course"),
+            )
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text('This is Home'),
+      ),
+    );
+  }
+}
